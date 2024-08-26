d99e2aa40ad439208b2b7187481e0af47b59e4ab
ahnshy <ahnshy@naver.com>
2024-08-26 09:02:18 +0900

    <feat> : Project Initialize and Add Dependency jar module

---
+++ src/main/java/com/github/ahnshy/SpringBbsApplication.java
@@ -0,0 +1,13 @@
+package com.github.ahnshy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ahnshy/SpringBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ahnshy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
