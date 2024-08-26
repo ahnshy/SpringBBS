e0c123ca494c307aafa265c4cd5f66010dd88178
ahnshy <ahnshy@naver.com>
2024-08-26 20:18:45 +0900

    <fix> : Remove gradle module without use and without dependency. <fix> : Add JUnit Default TestLogic

--- src/main/java/com/github/ahnshy/springbbs/SpringBbsApplication.java
+++ src/main/java/com/github/ahnshy/springbbs/SpringBbsApplication.java
@@ -1,12 +1,13 @@
-package com.github.ahnshy;
+package com.github.ahnshy.springbbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBbsApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBbsApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBbsApplication.class, args);
+	}
 
 }
---
+++ src/test/java/com/github/ahnshy/springbbs/SpringBbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ahnshy.springbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
